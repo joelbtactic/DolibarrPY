9e51d4075d785bcb1cbd933481a77bcacb8b5f49
joelbtactic <joel.ampurdanes@btactic.com>
2024-09-13 18:48:26 +0200

    Feat: add singleton.py

---
+++ singleton.py
@@ -0,0 +1,29 @@
+#######################################################################
+# Dolibarr PY is a simple Python client for Dolibarr API.
+
+# Copyright (C) 2023-2024 BTACTIC, SCCL
+# Copyright (C) 2023-2024 Joel Ampurdanés Bonjoch
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################
+
+
+class Singleton(object):
+
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
